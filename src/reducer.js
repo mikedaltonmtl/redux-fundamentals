5955711d21e3220cd6d41c5be74e772afc1ffc48
mike-dalton <michael.j.dalton@gmail.com>
2023-08-14 16:20:38 -0400

    slice reducer into features

--- src/reducer.js
+++ src/reducer.js
@@ -39,6 +39,39 @@ export default function appReducer(state = initialState, action) {
         ]
       }
     }
+    case 'todos/todoToggled': {
+      return {
+        // Again copy the entire state object
+        ...state,
+        // This time, we need to make a copy of the old todos array
+        todos: state.todos.map(todo => {
+          // If this isn't the todo item we're looking for, leave it alone
+          if (todo.id !== action.payload) {
+            return todo;
+          }
+
+          // We've found the todo that has to change. Return a copy:
+          return {
+            ...todo,
+            // Flip the completed flag
+            completed: !todo.completed
+          }
+        })
+      }
+    }
+    case 'filters/statusFilterChanged': {
+      return {
+        // Copy the whole state
+        ...state,
+        // Overwrite the filters value
+        filters: {
+          // copy the other filter fields
+          ...state.filters,
+          // And replace the status field with the new value
+          status: action.payload
+        }
+      }
+    }
     default:
       // If this reducer doesn't recognize the action type, or doesn't
       // care about this specific action, return the existing state unchanged
